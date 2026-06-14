60d5f6af773fdb811c858434970a309942eef44c
govind-geethub <avin14a@gmail.com>
2026-06-15 00:44:15 +0530

    worked on the shorten.go file

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,10 +1,14 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/govind-geethub/shorten-url-fiber-redis/database"
 	helpers "github.com/govind-geethub/shorten-url-fiber-redis/helper"
 )
 
@@ -33,6 +37,27 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	// implement rate limiting
 	// check the IP if exists then decrease its limit by 1
+
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	// new IP addr
+	if val, err := r2.Get(database.Ctx, c.IP()).Result(); err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		valInt, _ := strconv.Atoi(val)
+
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":            "Rate limit exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
+		}
+
+	}
+
+	// decrementing the rate
+	_ = r2.Decr(database.Ctx, c.IP())
 
 	// check if the input is the acutal url or not
 	if !govalidator.IsURL(body.URL) {
